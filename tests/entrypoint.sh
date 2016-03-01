af1b1bf376325912b950d88c24817f216b312765
Morgan Auchede <morgan.auchede@gmail.com>
2016-03-01 13:31:31 +0100

    Fix socket permission.
    Fix script "bin/test".
    
    Add "CHANGELOG.md".
    
    Improve syntax of "README.md".

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
 set -e
 
+# Install syslog-stdout
+
 tar fxz /dist/syslog-stdout.tar.gz -C /usr/sbin
+
+# Start syslog-stdout
 
 /usr/sbin/syslog-stdout &
 sleep 1
+
+pkill -9 syslog-stdout
+
+/usr/sbin/syslog-stdout &
+sleep 1
+
+# Test chmod
+
+echo $(stat -c '%a' /dev/log)
+
+# Test logs
 
 logger "Test 1 syslog-stdout"
 logger "Test 2 syslog-stdout"
